40830011ce5231a69224396fb4f8e0a99ee20291
Nika Gorski <nikamgorski@gmail.com>
2025-12-16 14:26:49 +0000

    reminder for the course

---
+++ course_exercises/info.R
@@ -0,0 +1,3 @@
+# this is a folder where we can keep all of the exercises that we do together as a class (so that they have a record of everything)
+
+# people mentioned wanting to practice with qualitative data (text), spatial data (maps), and introduce other data sources that they can use for their research
